9594eb910af57fbce42332676c08ab9cac15b4ca
Ivana <ivana_pile@yahoo.com>
2020-05-05 20:46:42 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function Scooter (year, color, model) {
+    this.year = year
+    this.color = color
+    this.model = model
+    //create a new scooter with three properties
+}
+
+function Driver(name, age, experience) {
+    this.name = name
+    this.age = age
+    this.experience = experience
+    //create a new driver with three properties
+}
+function PickupLocation(address, city) {
+    this.address = address
+    this.city = city
+    //create a new PickupLocation with two properties
+}
